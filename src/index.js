6c1c1c56d64e3275d33d6d356d1dfd9419f482a2
Joe France <joefrance@Joes-MacBook-Pro-15-Retina.local>
2021-03-11 13:09:30 -0500

    Initial repo.

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const readXlsxFile = require('read-excel-file/node');
+
+var myArgs = process.argv.slice(2);
+if(myArgs.length < 1) {
+    console.log("Usage: index.js /path/to/excelfile");
+    return;
+}
+console.log(myArgs[0]);
+
+// File path.
+readXlsxFile(myArgs[0]).then((rows) => {
+  // `rows` is an array of rows
+  // each row being an array of cells.
+  console.log(rows);
+}).catch((reason) => {
+    // `rows` is an array of rows
+    // each row being an array of cells.
+    console.log(reason);
+  })
